374cffd3f083b321f7f26d373091175fbe57b781
Lostovayne <moises.barrera.dev@gmail.com>
2023-07-20 16:19:28 -0400

    fix

--- public/js/imagenes.js
+++ public/js/imagenes.js
@@ -2,16 +2,41 @@ import { arrayImagenes } from "./functions.js";
 const section = document.querySelector(".container");
 
 window.addEventListener("DOMContentLoaded", function () {
-    arrayImagenes.forEach((element) => {
+    // arrayImagenes.forEach((element) => {
+    //     const imagen = document.createElement("img");
+    //     imagen.className = "imagen";
+    //     const div = document.createElement("div");
+    //     div.style.width = "250px";
+    //     div.style.height = "250px";
+    //     div.style.border = "1px solid #f2f2f2";
+    //     div.style.borderRadius = "8px";
+    //     div.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+    //     imagen.src = `../upload/${element}`;
+    //     div.appendChild(imagen);
+    //     section.appendChild(div);
+    // });
+
+    for (let i = 0; i < 8; i++) {
         const imagen = document.createElement("img");
         imagen.className = "imagen";
         const div = document.createElement("div");
-        div.style.width = "250px";
-        div.style.height = "250px";
+        div.style.width = "220px";
+        div.style.height = "220px";
         div.style.border = "1px solid #f2f2f2";
         div.style.borderRadius = "8px";
-        imagen.src = `../upload/${element}`;
-        div.appendChild(imagen);
+        div.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+        if (arrayImagenes[i]) {
+            imagen.src = `../upload/${arrayImagenes[i]}`;
+            imagen.addEventListener("click", () => {
+                window.location.href = `/deleteimg/${arrayImagenes[i]}`;
+            });
+            div.appendChild(imagen);
+        } else {
+            const p = document.createElement("p");
+            p.textContent = `${i + 1}`;
+            div.appendChild(p);
+        }
+
         section.appendChild(div);
-    });
+    }
 });
