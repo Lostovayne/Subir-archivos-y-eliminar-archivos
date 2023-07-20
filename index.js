d1de9839c461b789408771fd7dff7e394a78a27d
Lostovayne <moises.barrera.dev@gmail.com>
2023-07-20 12:24:15 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+import express from "express";
+import fileUpload from "express-fileupload";
+// import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import fs from "fs";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+app.use(
+    fileUpload({
+        limits: { fileSize: 50 * 1024 * 1024 },
+    })
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(express.json());
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/upload", (req, res) => {
+    const file = req.files.file;
+
+    if (req.files === null) {
+        return res.status(400).json({ msg: "No files were uploaded" });
+    }
+
+    if (file.size > 5000000) {
+        return res.status(400).json({ msg: "File size limit exceeded" });
+    }
+    file.mv(`${__dirname}/public/upload/${file.name}`, (err) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.json({ file: file.name });
+    });
+});
+
+app.get("/deleteimg/:nombre", (req, res) => {
+    const nombre = req.params.nombre;
+    fs.unlink(`${__dirname}/public/upload/${nombre}`, (err) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.json({ msg: "Imagen eliminada" });
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
